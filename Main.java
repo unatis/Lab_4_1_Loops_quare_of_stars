18db6c414e7621160493da0f3b959a9b3c8779f6
Yuri Goncharov <ygpanzer@gmail.com>
2021-04-17 15:26:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,22 @@
-erthgghr
+package com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int HeightSize = 5;
+        int WidthSize = 10;
+
+        for(int i = 0; i < HeightSize; i++){
+
+            for(int j = 0; j < WidthSize; j++){
+
+                System.out.print("*");
+
+            }
+
+            System.out.print("\n");
+        }
+
+    }
+}
